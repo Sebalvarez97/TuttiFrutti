d11773a0c896b446f093b37a9ac552d13d620099
Sebastian Alvarez Sanchez <salvarsa@usersad.everis.int>
2019-01-18 10:42:52 -0300

    Project

---
+++ Project/TuttiFrutti/src/main/java/com/students/everis/app/TuttiFruttiApplication.java
@@ -0,0 +1,14 @@
+package com.students.everis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuttiFruttiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuttiFruttiApplication.class, args);
+	}
+
+}
+
---
+++ Project/TuttiFrutti/src/test/java/com/students/everis/app/TuttiFruttiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.students.everis.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TuttiFruttiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
